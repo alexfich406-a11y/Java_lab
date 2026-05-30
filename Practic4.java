7a16146b275c461d3c463b07eeec622dfbcac963
alexfich406-a11y <alexfich406@gmail.com>
2026-05-31 02:39:36 +0300

    Create Practic4.java

---
+++ Practic4.java
@@ -0,0 +1,21 @@
+public class Practic4 {
+    public static void main(String[] args){
+        byte b = 10;
+        short sh = 20;
+        int i = 25;
+        long l = 40;
+        float fl = 50.4;
+        double db = 3.14;
+        boolean bl = true;
+        Object obj = new Object();
+        
+        String s1 = "Byte: " + b;
+        String s2 = "Short: " + sh;
+        String s3 = "Int: " + i;
+        String s4 = "Long: " + l;
+        String s5 = "Float: " + fl;
+        String s6 = "Double: " + db;
+        String s7 = "Bool: " + bl;
+        String s8 = "Object: " + obj;
+    }
+}
