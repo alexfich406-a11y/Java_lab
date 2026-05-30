52fe66ab54328c15eb9d9ae15e7644122ce41436
alexfich406-a11y <alexfich406@gmail.com>
2026-05-31 02:46:25 +0300

    Create Practic6.java

---
+++ Practic6.java
@@ -0,0 +1,28 @@
+public class Practic6 {
+
+    public static void main(String[] args){
+        int bigInt = 200;
+        System.out.println((byte) bigInt);   
+        
+        long bigLong = 333495869;
+        System.out.println((int) bigLong);
+        
+        double bigDouble = 9.99595039506;
+        System.out.println((int) bigDouble);
+        
+        float bigFloat = 130.9897f;
+        System.out.println((byte) bigFloat);
+        
+        char c = 'Я';
+        System.out.println((byte) c);
+        
+        short s = 200;
+        System.out.println((byte) s);
+        
+        double d = 1.2345345;
+        System.out.println((float) d);
+        
+        long l = 100000;
+        System.out.println((short) l);
+    }
+}
