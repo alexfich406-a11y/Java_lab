18f7803fff05b89b566299d91e71918555a4a097
alexfich406-a11y <alexfich406@gmail.com>
2026-05-31 02:51:54 +0300

    Create Practic1.java

---
+++ Practic1.java
@@ -0,0 +1,18 @@
+/**
+ * @author Student
+ * @version 1.0
+ * @since 2026-04-12
+ */
+public class Practic1 {
+
+    // Основное поле класса — целое число для демонстрации.
+    private int value = 42;
+   /**
+     * Метод возвращает текущее значение поля value.
+     *
+     * @return текущее значение типа int
+     */
+    public int getValue() {
+        return value;
+    }
+}
