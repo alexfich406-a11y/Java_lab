75252d8a8226f0542d4fab74a069302ba2cc15a7
alexfich406-a11y <alexfich406@gmail.com>
2026-05-31 02:19:47 +0300

    Create HomeWork1.java

---
+++ HomeWork1.java
@@ -0,0 +1,28 @@
+public class HomeWork1 {
+
+    public static void main(String[] args){
+        byte b = 10;
+        byte b0 = 0;
+        
+        short s = 20;
+        short s0 = 0;
+        
+        int i = 30;
+        int i0 = 0;
+        
+        long l = 40L;
+        long l0 = 0L;
+        
+        float f = 5.5f;
+        float f0 = 0.0f;
+        
+        double f = 6.6;
+        double f0 = 0.0;
+        
+        char c = 'A';
+        char c0 = '\u0000';
+        
+        boolean bl = true;
+        boolean bl0 = false;
+    }
+}
