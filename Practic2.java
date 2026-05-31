51b977c5cf7dafe8fad4736028b8bd22e8be35b7
alexfich406-a11y <alexfich406@gmail.com>
2026-05-31 13:22:40 +0300

    Create Practic2.java

---
+++ Practic2.java
@@ -0,0 +1,10 @@
+public class Practic2 {
+	  public static void main(String[] args){
+        Boolean b1 = Boolean.valueOf(true);
+        boolean b2 = Boolean.parseBoolean("true");
+        Boolean b3 = Boolean.valueOf("true");
+        Boolean b4 = Boolean.valueOf("false");
+        Boolean b5 = Boolean.valueOf("TRUE");
+        Boolean b6 = Boolean.valueOf("rand");
+    }
+}
