3daa94b744629dc656da825290ba80013c2bad71
alexfich406-a11y <alexfich406@gmail.com>
2026-05-31 14:54:27 +0300

    create Practic2.java

---
+++ Practic2.java
@@ -0,0 +1,31 @@
+class A {
+    int a;
+    int b;
+    int c;
+    int z;
+
+    public A() {
+        this(0, 0, 0);
+    }
+
+    public A(int a) {
+        this(a, 0, 0);
+    }
+
+    public A(int a, int b) {
+        this(a, b, 0);
+    }
+
+    public A(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        z = 1;
+    }
+}
+
+public class Practic2 {
+	  public static void main(String[] args){
+        
+    }
+}
