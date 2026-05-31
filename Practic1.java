9e482cbfb8ee2b06d35f0b413ab22be34589cf83
alexfich406-a11y <alexfich406@gmail.com>
2026-05-31 13:36:36 +0300

    Create Practic1.java

---
+++ Practic1.java
@@ -0,0 +1,11 @@
+public class Practic1 {
+	  public static void main(String[] args){
+        while (true) {
+            // тело цикла
+        }
+        
+        do {
+            // тело цикла
+        } while (true);
+    }
+}
