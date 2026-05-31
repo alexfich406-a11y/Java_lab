5e2dddc1bebf1cda8bef552a3261b909a74d1853
alexfich406-a11y <alexfich406@gmail.com>
2026-05-31 13:11:12 +0300

    Update to Practic1.java

--- Practic1.java
+++ Practic1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HomeWork2 {
+public class Practic1{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         String name = scan.next();
