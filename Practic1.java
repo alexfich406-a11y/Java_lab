ab2f3c0dbb0b64e8c62c0a31b491c9cb1306346f
alexfich406-a11y <alexfich406@gmail.com>
2026-05-31 20:32:33 +0300

    create Practic1.java

---
+++ Practic1.java
@@ -0,0 +1,10 @@
+public class Practic1 {
+
+    public static void main(String[] args){
+        int a = 2;
+        int i = 0;
+        i = (a == 1) ? 1 : (a == 2) ? 2 : 3;
+
+        System.out.println(i);
+    }
+}
